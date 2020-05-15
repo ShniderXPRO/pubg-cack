6d8c5fb7bf3a2a2eb7fa98baf92e5c58dd8c765d
ShniderXPRO <65216448+ShniderXPRO@users.noreply.github.com>
2020-05-15 18:40:40 -0500

    Add files via upload

---
+++ pubg-crack.py
@@ -0,0 +1,21 @@
+import os
+import time
+os.system("clear")
+time.sleep(3)
+os.system("figlet PUBG CRACK")
+dov = input ("Enter The Tool Password >>>>>>>>>> ")
+time.sleep(2)
+if dov == "dpxx441" :
+	v = input ("\033[1;32mEnter ID >>>>>>>>>> ")
+	time.sleep(5)
+	print ("\033[1;37mStart Cracking....!")
+	t = 1
+	time.sleep(t)
+	time.sleep(t)
+	time.sleep(t)
+	time.sleep(t)
+	print (" ")
+	print (" ")
+	print ("\033[0;32mPassword FOUND!!!\033[0;37m >>>>>>>>>>> \033[1;33m1133558800")
+	print (" ")
+	print (" ")
